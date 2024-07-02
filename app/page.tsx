296bf36884b99fbe585edf9f4f95f08f3b3a3f2b
IzaanAnwar <mdizaan67@gmail.com>
2024-07-03 01:59:32 +0530

    refactor: footer

--- app/page.tsx
+++ app/page.tsx
@@ -251,16 +251,7 @@ export default function Component() {
           </div>
         </section>
       </main>
-      <footer className="flex w-full shrink-0 flex-col items-center gap-2 px-4 py-6 sm:flex-row md:px-6">
-        <p className="text-xs dark:text-gray-700">
-          © 2024 Galzzy. All rights reserved.
-        </p>
-        <nav className="flex gap-4 sm:ml-auto sm:gap-6">
-          <Link className="text-xs underline-offset-4 hover:underline" href="#">
-            Privacy Policy
-          </Link>
-        </nav>
-      </footer>
+     
     </div>
   );
 }
